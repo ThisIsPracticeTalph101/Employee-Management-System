4c97b4c0c5007c0322491350450f8c01ba17ce98
Tiffany <139176913+talph101@users.noreply.github.com>
2024-10-24 17:12:11 -0500

    Created all REST APIs that handle CRUD operations.

--- src/main/java/com/tiffanysproject/ems/controller/EmployeeController.java
+++ src/main/java/com/tiffanysproject/ems/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @AllArgsConstructor //to not have to create a constructor manually
 @RestController
 @RequestMapping("/api/employees")
@@ -19,4 +21,35 @@ public class EmployeeController {
         EmployeeDTO savedEmployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
+
+    //Build GET Employee REST API
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDTO> getEmployeeId(@PathVariable("id") Long employeeId){
+        EmployeeDTO employeeDto = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    //Build GET all Employees REST API
+    @GetMapping
+    public ResponseEntity<List<EmployeeDTO>> getAllEmployees(){
+        List<EmployeeDTO> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+
+    }
+
+    //Build UPDATE Employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDTO> updateEmployee(@PathVariable("id") Long employeeId, @RequestBody EmployeeDTO updatedEmployee){
+        EmployeeDTO employeeDTO= employeeService.updateEmployee(employeeId, updatedEmployee);
+        return ResponseEntity.ok(employeeDTO);
+    }
+
+    //Build DELETE Employee REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id){
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.ok("Employee deleted successfully!");
+    }
+
+
 }
