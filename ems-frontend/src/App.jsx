6f6f885ad8e895ec3ff4178a2a8a13ff56dc144c
Tiffany <139176913+talph101@users.noreply.github.com>
2024-10-25 12:27:34 -0500

    Initial commit with backend and frontend

---
+++ ems-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+//removed default code
+//App is where to inject all components
+
+import './App.css'
+import HelloWorld from './HelloWorld'
+
+function App() {
+ 
+
+  return (
+    <>
+     <HelloWorld/>
+    </>
+  )
+}
+
+export default App
